95cd9495e747361e79fb4a357b4dc639488feaa2
vittorlk <vitorgotpos@gmail.com>
2025-12-29 09:46:00 -0300

    teste - botão

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuBtn = document.getElementById('menu-btn');
-    const nav = document.getElementById('navpastel');
-
-    if (menuBtn && nav) {
-        menuBtn.addEventListener('click', () => {
-            nav.classList.toggle('active');
-        });
-    }
-});
-
-// Nova Logica 
-
-document.addEventListener('DOMContentLoaded', () => {
 
     // =========================
     // MENU MOBILE
